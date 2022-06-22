ccb739b42869a2265fd9130a203b0deb91da12e1
obordonado <obordonado@protonmail.com>
2022-06-22 20:27:39 +0200

    estructura web

--- proyecto-app/src/App.jsx
+++ proyecto-app/src/App.jsx
@@ -1,34 +1,30 @@
 
-
 import { useState } from "react";
-// import { BrowserRouter, Routes } from "react-router-dom"
-// import Header from './components/Header/Header'
 import "./App.css"
 
 function App() {
   return (
 
-  //     <BrowserRouter className="browser">
-  //       <div className="home">
-  //         <div className="header">
-  //         </div>
+        <div className="home">
+          <div className="header">
+          </div>
         
 
-  //         <div className="middle">
-  //           <div className="left"></div>
+          <div className="middle">
+            <div className="left">
+              
+            </div>
 
-  //           <div className="right">
-  //             <Routes>
-  //             </Routes>
-  //           </div>
-  //         </div>
+            <div className="right">
+            
+            </div>
+          </div>
 
-  //         <Footer className="footer" />
-  //       </div>
-  //     </BrowserRouter>
-  // );
-  <div className="middle"></div>)
-  
+          <div className="footer" />
+        </div>
+
+  );
+
 }
 
 export default App;
