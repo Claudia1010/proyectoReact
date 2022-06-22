22cdce08c564329f16140598ab6a30d1778a53dc
obordonado <obordonado@protonmail.com>
2022-06-22 23:54:21 +0200

    terminado e incluídos los componentes en App.jsx

--- proyecto-app/src/App.jsx
+++ proyecto-app/src/App.jsx
@@ -1,30 +1,32 @@
-
 import { useState } from "react";
-import "./App.css"
-
+import "./App.css";
+import "./components/Header/Header";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Left from "./components/Left/Left";
+import Right from "./components/Right/Right";
 function App() {
   return (
+    <div className="home">
+      <div className="header">
+        <Header />
+      </div>
 
-        <div className="home">
-          <div className="header">
-          </div>
-        
-
-          <div className="middle">
-            <div className="left">
-              
-            </div>
-
-            <div className="right">
-            
-            </div>
-          </div>
-
-          <div className="footer" />
+      <div className="middle">
+        <div className="left">
+          <Left />
         </div>
 
-  );
+        <div className="right">
+          <Right />
+        </div>
+      </div>
 
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
