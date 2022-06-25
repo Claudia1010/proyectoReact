a132ed41e0ac9dd14de8b4a578f8b77c76d2becb
Alexlg <Alejandrolg86@gmail.com>
2022-06-25 17:53:29 +0200

    creating login container

--- proyecto-app/src/container/Login/Login.jsx
+++ proyecto-app/src/container/Login/Login.jsx
@@ -1,13 +1,45 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./Login.css"
 
 const Login = () => {
-    return(
+
+    const [userData, setUserData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handlerForm = (event) => {
+        setUserData({
+            ...userData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const loginFunction = async () => {
+
+    }
+
+    return (
+        
         <div className="loginStyles">
-            Login
+
+            {/* <pre>{JSON.stringify(userData, null, 2)}</pre> */}
+
+            <div>
+                <input className="email" type="text" label="Email" name="email" placeholder="E-mail " onChange={(event) => {handlerForm(event)}} />
+            </div>
+            
+            <div>
+                <input className="password" type="text" label="Password" name="password" placeholder="Password " onChange={(event) => {handlerForm(event)}} />
+            </div>
+            
+            <div>
+                <button className="loginButtom" type="submit" target="_self" onClick={() => loginFunction()}>Log in</button>
+            </div>
         </div>
+
     )
-    
+
 }
 
 export default Login
