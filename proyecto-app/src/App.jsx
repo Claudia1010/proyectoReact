6097848711ca152ea25c1bfb2099c8fd1adc66ae
Alexlg <Alejandrolg86@gmail.com>
2022-06-25 11:07:37 +0200

    adding browser router and routes

--- proyecto-app/src/App.jsx
+++ proyecto-app/src/App.jsx
@@ -1,27 +1,31 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { useState } from "react";
 import "./App.css"
 
 function App() {
   return (
 
-        <div className="home">
-          <div className="header">
-          </div>
-        
-
-          <div className="middle">
-            <div className="left">
-              
-            </div>
-
-            <div className="right">
-            
-            </div>
-          </div>
-
-          <div className="footer" />
+    <div className="main">
+      <BrowserRouter>
+        <div className="header">
         </div>
+        <div className="middle">
+          <div className="left">
+          </div>
+          <div className="right">
+            {/* <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/Login" element={<Login/>}/>
+              <Route path="/Register" element={<Register/>}/>
+              <Route path="/RentList" element={<RentList/>}/>
+              <Route path="/TotalRentList" element={<TotalRentList/>}/>
+              <Route path="/NotFound" element={<NotFound/>}/>
+            </Routes> */}
+          </div>
+        </div>
+        <div className="footer" />
+      </BrowserRouter>
+    </div>
 
   );
 
